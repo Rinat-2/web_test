4b5018652896bf9fc34a785350a5d67a9ed2b737
Rinat <rinat.astr2@gmail.com>
2020-10-26 23:25:14 +0600

    Added property for Jenkins

--- src/test/java/helpers/PropertiesHelper.java
+++ src/test/java/helpers/PropertiesHelper.java
@@ -2,6 +2,7 @@ package helpers;
 
 import org.aeonbits.owner.Config;
 
+//
 @Config.Sources({"classpath:local.properties" , "classpath:remote.properties"})
 public interface PropertiesHelper extends Config {
 
